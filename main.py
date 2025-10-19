1f5633c1da42ab39a200ad9b02e8c93c2789b7e7
aaawosjdjdi <1115213530@qq.com>
2025-10-19 10:09:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ b = 0
 c = 0
 d = 0
 
-for i in s:
-    if i >= 'a' and i <= 'z' or i >= 'A' and i <= 'Z':
-        a = a + 1
-    elif i >= '0' and i <= '9':
-        b = b + 1
-    elif i == ' ':
-        c = c + 1
+for char in s:
+    if char.isalpha():
+        a += 1
+    elif char.isdigit():
+        b += 1
+    elif char.isspace():
+        c += 1
     else:
-        d = d + 1
+        d += 1
 
 print("英文字符:", a)
 print("数字:", b)
