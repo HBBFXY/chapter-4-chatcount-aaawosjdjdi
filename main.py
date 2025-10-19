c1e32ce9fd04d1140e81ea5a5661746dbb456cdc
aaawosjdjdi <1115213530@qq.com>
2025-10-19 10:07:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-text = input()
+s = input()
+a, b, c, d = 0, 0, 0, 0
 
-letters = 0
-digits = 0
-spaces = 0
-others = 0
-
-for char in text:
-    if char.isalpha():
-        letters += 1
-    elif char.isdigit():
-        digits += 1
-    elif char.isspace():
-        spaces += 1
+for i in s:
+    if i.isalpha():
+        a += 1
+    elif i.isdigit():
+        b += 1
+    elif i == ' ':
+        c += 1
     else:
-        others += 1
+        d += 1
 
-print("英文字符:", letters)
-print("数字:", digits)
-print("空格:", spaces)
-print("其他字符:", others)
+print("英文字符:", a)
+print("数字:", b)
+print("空格:", c)
+print("其他字符:", d)
